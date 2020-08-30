2262c1140955c2f1c4192592f08de6ce70bb4755
Sithija Shehara <nsithijashehara@gmail.com>
2020-08-30 23:01:14 +0530

    basic task table with tab bar

--- My-To-Do/src/app/home-page/home-page.module.ts
+++ My-To-Do/src/app/home-page/home-page.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page-routing-module';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
     HomePageRoutingModule
   ],
   declarations: [HomePageComponent]
