4cdbc4372dde4b32a8fe6e4c6a738044703a1bd7
Sithija Shehara <nsithijashehara@gmail.com>
2020-09-01 09:03:38 +0530

    Hardcoded task-list with customized checkbox

--- My-To-Do/src/app/home-page/home-page.module.ts
+++ My-To-Do/src/app/home-page/home-page.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page-routing-module';
-import { MatCardModule, MatButtonModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
+    MatListModule,
     HomePageRoutingModule
   ],
   declarations: [HomePageComponent]
