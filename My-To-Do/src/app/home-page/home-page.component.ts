e1ea4e50a70818002db354e42557a6d893cb625b
Sithija Shehara <nsithijashehara@gmail.com>
2020-09-05 13:17:42 +0530

    button enabling

--- My-To-Do/src/app/home-page/home-page.component.ts
+++ My-To-Do/src/app/home-page/home-page.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  categories = [
+    {title:'Personal', active:true},
+    {title:'Work', active:false}
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  /* function of enable categories */
+  openCategory(category) {
+    this.markAllCategories(category);
+    category.active = true;
+  }
+
+  /* mark all categories */
+  markAllCategories(category) {
+    this.categories.map((category)=>{
+      category.active = false;
+      return category;
+    })
+  }
+
 
 }
