5049c431e2a89a3bbcfdcce56b04d1ccdb6c7dcb
Sithija Shehara <nsithijashehara@gmail.com>
2021-01-02 09:16:08 +0530

    Added morgan and error handling.

---
+++ NodeJS/Academind/api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/',(req,res) => {
+    res.status(200);
+    res.send('get request to products');
+})
+
+router.post('/', (req,res) => {
+    res.status(200);
+    res.send('post request to products');
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+module.exports = router;
